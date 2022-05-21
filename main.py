079f5ab5a2457ae9799a7da0ca5521d14cfcdebf
Eugene <eugenechoi012@gmail.com>
2022-05-21 13:12:29 -0400

    i dont know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import Widget
 import pygame
+from pygame.locals import *
 import sys
 import math
 import random
@@ -55,9 +56,54 @@ class Player(pygame.sprite.Sprite):
         self.vel = vec(0,0) 
         self.acc = vec(0,0)
         self.direction = "RIGHT"
+
+        # Movement 
+        self.jumping = False
+        self.running = False
+        self.move_frame = 0
+
     def move(self):
-        pass
- 
+        # Keep a constant acceleration of 0.5 in the downwards direction (gravity)
+        self.acc = vec(0,0.5)
+
+        # Will set running to False if the player has slowed down to a certain extent
+        if abs(self.vel.x) > 0.3:
+            self.running = True
+        else:
+            self.running = False
+        # Returns the current key presses
+        pressed_keys = pygame.key.get_pressed()
+    
+        # Accelerates the player in the direction of the key press
+        if pressed_keys[K_LEFT]:
+                self.acc.x = -ACC
+                self.direction = "LEFT"
+        if pressed_keys[K_RIGHT]:
+                self.acc.x = ACC 
+                self.direction = "RIGHT"
+    
+        # Formulas to calculate velocity while accounting for friction
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc  # Updates Position with new values
+    
+        # This causes character warping from one point of the screen to the other
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        
+        self.rect.midbottom = self.pos  # Update rect with new pos 
+    def gravity_check(self):
+        hits = pygame.sprite.spritecollide(player ,ground_group, False)
+        if self.vel.y > 0:
+            if hits:
+                lowest = hits[0]
+                if self.pos.y < lowest.rect.bottom:
+                    self.pos.y = lowest.rect.top + 1
+                    self.vel.y = 0
+                    self.jumping = False
+
     def update(self):
         pass
     
@@ -65,8 +111,17 @@ class Player(pygame.sprite.Sprite):
         pass
     
     def jump(self):
-        pass
-     
+        self.rect.x += 1
+    
+        # Check to see if payer is in contact with the ground
+        hits = pygame.sprite.spritecollide(self, ground_group, False)
+        
+        self.rect.x -= 1
+    
+        # If touching the ground, and not currently jumping, cause the player to jump.
+        if hits and not self.jumping:
+            self.jumping = True
+            self.vel.y = -12
  
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -76,6 +131,11 @@ class Enemy(pygame.sprite.Sprite):
 background = Background()
 ground = Ground()
 player = Player()
+
+ground = Ground()
+ground_group = pygame.sprite.Group()
+ground_group.add(ground)
+
 while True:
     for event in pygame.event.get():
         # Will run when the close window button is clicked    
@@ -86,9 +146,15 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pass
  
-        # Event handling for a range of different key presses    
+        # Event handling for a range of different key presses
         if event.type == pygame.KEYDOWN:
-            pass
+           if event.key == pygame.K_SPACE:
+                  player.jump()
+
+    # update player
+
+    player.gravity_check()
+    player.move()
 
     # Render Functions ------
     background.render()
